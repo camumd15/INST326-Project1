290e4891a135e2283c980d7891582647f4df0962
jrayela326 <john_rayela@yahoo.com>
2021-04-01 18:03:01 -0400

    added instproject.py

--- instproject.py
+++ instproject.py
@@ -4,11 +4,16 @@ available"""
 class Menu:
     """this class will have the lists of items on the menu to search 
     through when called
+    
     Attributes:
+    
     Sides (str): will list side options for meals
-    Entree: Will show the option of the entrees that are on the menu
-    Dessert (str): Will show a list of the desserts available on the menu"""
+    Entree (set of food): Will show the option of the entrees that are on the menu
+    Dessert (str): Will show a list of the desserts available on the menu
+    """
     
     def __init__(self, food):
+        """ Initialize a new Menu object. """
         self.food = food
+        self.entrees = set()
         
